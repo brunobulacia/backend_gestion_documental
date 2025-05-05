4d638a45f4a7d8499fc14a5de4bf6eb0e4e1af4c
Bruno Bulacia <beueleceia@gmail.com>
2025-05-04 20:58:27 -0400

    🔧 Update database settings and add serializers for user roles and permissions

--- backend/settings.py
+++ backend/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+} """
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "gestion_documental",
+        "USER": "postgres",
+        "PASSWORD": "GRUPO9SI2",
+        "HOST": "db-gestion-documental-si2.c1gcgqmkyd8a.sa-east-1.rds.amazonaws.com",
+        "PORT": "5432"
     }
 }
 
--- documentos/admin.py
+++ documentos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Documento, DocumentoVersion, PermisoDocumento, TipoDocumento, Area, MetadatoPersonalizado, ComentarioDocumento
+
+admin.site.register(Documento)
+admin.site.register(DocumentoVersion)
+admin.site.register(PermisoDocumento)
+admin.site.register(TipoDocumento)
+admin.site.register(Area)
+admin.site.register(MetadatoPersonalizado)
+admin.site.register(ComentarioDocumento)
